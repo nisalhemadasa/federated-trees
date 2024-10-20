25aa191a65550369b386fab5f54aa26cd3034426
LAP-ONE\nisalhem <nisal.hemadasa@tuhh.de>
2024-10-20 16:33:40 +0200

    Modified the main.py to create federated_network instance.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+"""
+This is the main entry point for the simulation. You can run this script
 
+Author: Nisal Hemadasa
+Date: 19-10-2024
+Version: 1.0
+"""
+from federated_network.network import FederatedNetwork
+
+
+def main():
+    # Cretaing the federated network instance
+    fed_net = FederatedNetwork()
+
+    # Running the simulation
+    fed_net.run_simulation()
+
+
+if __name__ == "__main__":
+    main()
